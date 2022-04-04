18662e7bc37da4aec83e6a42c3d4b6b3855ec680
Yago Russo <yagrrusso@gmail.com>
2022-04-04 18:09:25 -0300

    netlify router

--- src/components/AsideItem/index.tsx
+++ src/components/AsideItem/index.tsx
@@ -2,12 +2,6 @@ import { useNavigate, useLocation } from 'react-router-dom'
 
 import * as C from './styles'
 
-import HomeFillImg from '../../images/icons/house-fill.svg'
-import StackFillImg from '../../images/icons/layers-fill.svg'
-import EditFillImg from '../../images/icons/pencil-fill.svg'
-import CvFillImg from '../../images/icons/cv-fill.svg'
-import ProfileFillImg from '../../images/icons/person-fill.svg'
-
 type Props = {
     name: string,
     route: string,

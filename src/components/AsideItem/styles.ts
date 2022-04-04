7f2273b3f924c083301c36a5f3f6e32439f3be94
Yago Russo <yagrrusso@gmail.com>
2022-04-04 12:58:30 -0300

    template lindo pronto e funcionando

--- src/components/AsideItem/styles.ts
+++ src/components/AsideItem/styles.ts
@@ -5,8 +5,6 @@ export const Container = styled.li<{ active: boolean }>`
     align-items: center;
     flex-direction: column;
     cursor: pointer;
-    color: ${props => props.active ? props.theme.primaryColor : props.theme.color};
-    font-weight: ${props => props.active ? 'bold' : '500'};
     padding: 20px 0;
     background-color: ${props => props.active ? props.theme.backgroundColor : ''};
     padding-right: 6px;
@@ -22,5 +20,11 @@ export const Container = styled.li<{ active: boolean }>`
         margin-bottom: 5px;
         height: 32px;
         width: auto;
+    }
+
+    span{
+        color: ${props => props.active ? props.theme.primaryColor : props.theme.color};
+        font-weight: ${props => props.active ? 'bold' : '500'};
+        text-transform: uppercase;
     }
     `

c9c52dc4c6dcc059395f3c993df500308e4f6d42
Yago Russo <yagrrusso@gmail.com>
2022-04-04 18:04:39 -0300

    teste

--- src/components/AsideItem/index.tsx
+++ src/components/AsideItem/index.tsx
@@ -2,6 +2,12 @@ import { useNavigate, useLocation } from 'react-router-dom'
 
 import * as C from './styles'
 
+import HomeFillImg from '../../images/icons/house-fill.svg'
+import StackFillImg from '../../images/icons/layers-fill.svg'
+import EditFillImg from '../../images/icons/pencil-fill.svg'
+import CvFillImg from '../../images/icons/cv-fill.svg'
+import ProfileFillImg from '../../images/icons/person-fill.svg'
+
 type Props = {
     name: string,
     route: string,
@@ -15,7 +21,7 @@ export const AsideItem = ({ name, route, img }: Props) => {
 
     return (
         <C.Container onClick={() => navigate(route)} active={isActive}>
-            <img src={isActive ? img.replace('assets/', 'assets/fill-') : img} alt={name} />
+            <img src={isActive ? img.replace('.svg', '-fill.svg') : img} alt={name} />
             <span>{name}</span>
         </C.Container>
     )

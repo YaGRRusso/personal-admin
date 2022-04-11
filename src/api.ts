bc7fc53a46d8083dcc2ad0d7cbf23586f0f73192
Yago Russo <yagrrusso@gmail.com>
2022-04-11 10:46:14 -0300

    teste

--- src/api.ts
+++ src/api.ts
@@ -1,18 +1,18 @@
 import axios from "axios"
 
-const baseURL = 'https://api.imgur.com/3/image/'
+const baseURL = 'https://api.imgur.com/3/image'
 
 export const api = {
     sendImage: async (image: File) => {
         let response = await axios({
-            url: baseURL,
             method: 'POST',
+            url: baseURL,
             headers: {
-                Authorization: 'Client-ID a4895ad056588bd'
+                'Authorization': 'Client-ID a4895ad056588bd'
             },
             data: image
         })
-        console.log(response)
+        console.log(JSON.stringify(response.data))
         return response
     }
 }
